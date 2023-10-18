69b89a8d67e96161966b29d18b93d43befdf7f5c
Matheus <matheusp-2014@outlook.com.br>
2023-10-18 10:08:11 -0300

    make it better

---
+++ express.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname));
+app.get('/', function(req, res){
+    res.sendFile('index.html');
+});
+app.listen(process.env.PORT || 3000);
